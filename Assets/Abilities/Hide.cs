f2540c7d10dd66263e4715a84716212a02ab2909
TNAGameStudio <144418379+TNAGameStudio@users.noreply.github.com>
2023-09-14 21:35:52 -0700

    Vanishing effect

--- Assets/Abilities/Hide.cs
+++ Assets/Abilities/Hide.cs
@@ -12,24 +12,24 @@ public class Hide : NetworkBehaviour
      [SerializeField] Player ourPlayer;
      Renderer _renderer;
      private StarterAssetsInputs _input;
-     int HIDING_LAYER = 6;
-     int UNHIDING_LAYER = 0;
      bool isHiding = false;
      float HideCooldown = 3.0f; // 3 seconds
      bool onCooldown = false;
 
-    public Material m_hide_body;
-    public Material m_hide_arms;
-    public Material m_hide_legs;
-    public Material m_unhide_body;
-    public Material m_unhide_arms;
-    public Material m_unhide_legs;
+    Shader transparent;
+    Shader originalShader;
 
+            
      
     // Start is called before the first frame update
     void Start()
     {
         _input = GetComponent<StarterAssetsInputs>();
+        
+            //transparent = Shader.Find("Shader Graphs/Vanish"); // vanish shader
+            transparent = Shader.Find("Shader Graphs/Dissolve"); // dissolve
+            if( transparent == null)
+                Debug.Log("Could not find the vanish shader.");
     }
 
     void Update()
@@ -93,8 +93,6 @@ public class Hide : NetworkBehaviour
     [ServerRpc]
     private void HidePlayerServerRpc(bool isHiding, ulong clientid)
     {
-        Debug.Log("In HidePlayerServerRPC Client ID: " + clientid + " Owner id: " + OwnerClientId);
-
         // If it's anyone else but the host
         if(clientid != 0)
         {
@@ -106,15 +104,6 @@ public class Hide : NetworkBehaviour
                 Debug.Log("Found the other player with client id: " + clientid + " going to hide them now.");
             }
 
-            if(isHiding)// hide them
-            {
-                SetHidingLayerMaskRecursively(hidingPlayer.gameObject, HIDING_LAYER);
-            }
-            else
-            {
-                SetHidingLayerMaskRecursively(hidingPlayer.gameObject, UNHIDING_LAYER);
-            }
-
             // play animation of opposing player as they hide or unhide
             PlayHideUnhideAnimation(hidingPlayer, isHiding);
 
@@ -124,14 +113,30 @@ public class Hide : NetworkBehaviour
         HidePlayerClientRpc(isHiding, clientid);
     }
 
+    [ClientRpc]
+    private void HidePlayerClientRpc(bool isHiding, ulong clientid)
+    {
+        // Do not perform on ourselves.
+        if(IsOwner)
+            return;
+
+    
+        if(ourPlayer.playerList.TryGetValue(clientid, out Player hidingPlayer))
+        {
+            // TODO Handle any errors (though this is success case)
+        } 
+
+        // play animation of opposing player as they hide or unhide
+        PlayHideUnhideAnimation(hidingPlayer, isHiding);                
+
+    }
+
     private void PlayHideUnhideAnimation(Player hidingPlayer, bool isHiding)
     {
         
 
         if(isHiding)
         {
-
-
             StartCoroutine(FadeOut(hidingPlayer));
 
         }
@@ -144,18 +149,40 @@ public class Hide : NetworkBehaviour
         
 
     }
+
     IEnumerator FadeOut(Player hidingPlayer)
     {
         Renderer _renderer = hidingPlayer.GetComponentInChildren<Renderer>();
-        Material[] hideMaterials = new Material[3];
 
-        hideMaterials[0] = m_hide_body;
-        hideMaterials[1] = m_hide_arms;
-        hideMaterials[2] = m_hide_legs;
+        //save original shader
+        originalShader = _renderer.materials[0].shader;
+        
+        // assign new shader to all materials
+        for(int i = 0; i < _renderer.materials.Length; i++)
+        {
+            _renderer.materials[i].shader = transparent;
+        }
 
-        _renderer.materials = hideMaterials;
+        // transition the transparency
+        for (float alpha = 1.0f; alpha >= 0; alpha -= 0.01f)
+        {
+            _renderer.materials[0].SetFloat("_Progress", alpha);
+            _renderer.materials[1].SetFloat("_Progress", alpha);
+            _renderer.materials[2].SetFloat("_Progress", alpha);
+            yield return new WaitForSeconds(.01f);
+        }
+        
+        // round off any inprecision
+        _renderer.materials[0].SetFloat("_Progress", 0);
+        _renderer.materials[1].SetFloat("_Progress", 0);
+        _renderer.materials[2].SetFloat("_Progress", 0);
+    }
 
+    IEnumerator FadeIn(Player hidingPlayer)
+    {
+        Renderer _renderer = hidingPlayer.GetComponentInChildren<Renderer>();
 
+        // transition the transparency
         for (float alpha = 0.0f; alpha < 1; alpha += 0.01f)
         {
 
@@ -171,67 +198,15 @@ public class Hide : NetworkBehaviour
         _renderer.materials[1].SetFloat("_Progress", 1);
         _renderer.materials[2].SetFloat("_Progress", 1);
 
-    }
-    IEnumerator FadeIn(Player hidingPlayer)
-    {
-        Renderer _renderer = hidingPlayer.GetComponentInChildren<Renderer>();
-        Material[] hideMaterials = new Material[3];
-
-        for (float alpha = 1.0f; alpha > 0; alpha -= 0.01f)
+        //restore original shader
+        for(int i = 0; i < _renderer.materials.Length; i++)
         {
-            Debug.Log("Fading in " + alpha);
-            _renderer.materials[0].SetFloat("_Progress", alpha);
-            _renderer.materials[1].SetFloat("_Progress", alpha);
-            _renderer.materials[2].SetFloat("_Progress", alpha);
-            yield return new WaitForSeconds(.01f);
+            _renderer.materials[i].shader = originalShader;
         }
-
-        // round off any inprecision
-        _renderer.materials[0].SetFloat("_Progress", 0);
-        _renderer.materials[1].SetFloat("_Progress", 0);
-        _renderer.materials[2].SetFloat("_Progress", 0);
-
-        hideMaterials[0] = m_unhide_body;
-        hideMaterials[1] = m_unhide_arms;
-        hideMaterials[2] = m_unhide_legs;
-
-        _renderer.materials = hideMaterials;
     }
 
-    private void SetHidingLayerMaskRecursively(GameObject playerGO, int masklayer)
-    {
-        //DEBUG
-        return;
 
-        playerGO.layer = masklayer;
-        foreach (Transform child in playerGO.transform)
-        {
-            SetHidingLayerMaskRecursively(child.gameObject, masklayer);
-        }
-    }    
 
-    [ClientRpc]
-    private void HidePlayerClientRpc(bool isHiding, ulong clientid)
-    {
-        // Do not perform on ourselves.
-        if(IsOwner)
-            return;
-
-    
-            if(ourPlayer.playerList.TryGetValue(clientid, out Player hidingPlayer))
-            {
-                // TODO Handle any errors (though this is success case)
-            } 
-
-            if(isHiding)// hide them
-                SetHidingLayerMaskRecursively(hidingPlayer.gameObject, HIDING_LAYER);
-            else
-                SetHidingLayerMaskRecursively(hidingPlayer.gameObject, UNHIDING_LAYER);
-
-            // play animation of opposing player as they hide or unhide
-            PlayHideUnhideAnimation(hidingPlayer, isHiding);                
-
-    }
 
 
 }
